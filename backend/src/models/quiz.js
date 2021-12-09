84dd4ea87dbec9e19c87caeb992079e11a01b377
khanhnd <khanhnd.itmo@gmail.com>
2021-12-10 00:01:42 +0300

    update quiz model

--- backend/src/models/quiz.js
+++ backend/src/models/quiz.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
+
 const { singleChoiceSchema, multipleChoiceSchema, fillInTheBlankSchema } = require('./question');
+const User = require('./user');
+
+const recordSchema = new mongoose.Schema({
+    user: { type: User },
+    score: { type: 'Number' }
+});
 
 const quizSchema = new mongoose.Schema({
-    questions: { type: Array.from([singleChoiceSchema, multipleChoiceSchema, fillInTheBlankSchema]) }
+    questions: { type: Array.from([singleChoiceSchema, multipleChoiceSchema, fillInTheBlankSchema]) },
+    contestants: { type: recordSchema }
 });
 
 module.exports = mongoose.model('quiz', quizSchema);
