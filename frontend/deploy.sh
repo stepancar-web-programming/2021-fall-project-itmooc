59ab6f25dddae3cc0f4e6fb0856376088a54d17c
Duy Khanh Nguyen <65860824+khanhnd0908@users.noreply.github.com>
2022-01-17 01:28:36 +0800

    move docker build before docker stop

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -12,12 +12,12 @@ DOCKER_IMAGE="itmooc_frontend"
 CONAINER_NAME="itmooc_frontend"
 SERVICE_PORT=3000
 
+docker build -t $DOCKER_IMAGE .
+
 #Check for running container & stop it before starting a new one
 if [ $(docker inspect -f '{{.State.Running}}' $CONAINER_NAME) = "true" ]; then
     docker stop $CONAINER_NAME
 fi
-
-docker build -t $DOCKER_IMAGE .
 
 docker run -d --rm=true -p $SERVICE_PORT:$SERVICE_PORT --name $CONAINER_NAME $DOCKER_IMAGE
 
