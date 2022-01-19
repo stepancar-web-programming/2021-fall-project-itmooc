ad5fd7a6f10556ed3b252b7f9db102f447a0de9e
khanhnd0908 <khanhnd.itmo@gmail.com>
2022-01-20 03:14:12 +0800

    prod env

---
+++ frontend/src/modules/auth/constants_prod.js
@@ -0,0 +1,4 @@
+export const LOGIN_URL = `http://159.223.68.98:4001/api/v1/sign-in`;
+export const SIGN_UP_URL = `http://159.223.68.98:4001/api/v1/sign-up`;
+export const UPDATE_INFO_URL = `http://159.223.68.98:4001/api/v1/update-info`;
+export const ME_URL = `http://159.223.68.98:4001/api/v1/me`;
---
+++ frontend/src/modules/home/constants_prod.js
@@ -0,0 +1,2 @@
+export const JOIN_URL = `http://159.223.68.98:4001/api/v1/join`;
+export const JOIN_PASSWORD_URL = `http://159.223.68.98:4001/api/v1/join-password`;
